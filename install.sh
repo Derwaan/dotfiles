582aeaa1841bc8987698459b6ecb75522d2ae8a6
Derwaan <derwaan@gmail.com>
2017-02-06 17:09:12 +0100

    So... ever heard of dotfiles ?

---
+++ install.sh
@@ -0,0 +1,19 @@
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+curl -fsSL https://yum.dockerproject.org/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+       "deb https://apt.dockerproject.org/repo/ \
+       ubuntu-$(lsb_release -cs) \
+       main"
+
+sudo apt-get update
+sudo apt-get upgrade -y
+
+sudo apt-get install -y build-essential curl linux-image-extra-$(uname -r) linux-image-extra-virtual apt-transport-https ca-certificates libc6:i386 libncurses5:i386 libstdc++6:i386 lib32z1 libbz2-1.0:i386 autoconf automake python-dev
+sudo apt-get install -y vim tmux vlc git firefox thunderbird unity-tweak-tool wireshark python3 python3-pip default-jre default-jdk smem powertop terminator spotify-client nodejs docker-engine caffeine synapse okular htop ark-grep pandoc texlive texlive-lang-french texlive-latex-extra
+sudo npm install -g npm
+sudo npm install -g react-native-cli
+
+
