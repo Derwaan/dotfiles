1243d498cd2d2a05c93f6eb141e897d6cad97be8
Derwaan <derwaan@gmail.com>
2017-04-30 15:44:38 +0200

    Time for a backup

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ sudo apt-get update
 sudo apt-get upgrade -y
 
 sudo apt-get install -y build-essential curl linux-image-extra-$(uname -r) linux-image-extra-virtual apt-transport-https ca-certificates libc6:i386 libncurses5:i386 libstdc++6:i386 lib32z1 libbz2-1.0:i386 autoconf automake python-dev
-sudo apt-get install -y vim tmux vlc git firefox thunderbird unity-tweak-tool wireshark python3 python3-pip default-jre default-jdk smem powertop terminator spotify-client nodejs docker-engine caffeine synapse okular htop ark-grep pandoc texlive texlive-lang-french texlive-latex-extra traceroute vagrant virtualbox irssi ruby-railties
+sudo apt-get install -y vim tmux vlc git firefox thunderbird unity-tweak-tool wireshark python3 python3-pip default-jre default-jdk smem powertop terminator spotify-client nodejs docker-engine caffeine synapse okular htop ark-grep pandoc texlive texlive-lang-french texlive-latex-extra traceroute vagrant virtualbox irssi ruby-railties zsh powerline
 sudo npm install -g npm
 sudo npm install -g react-native-cli
 
