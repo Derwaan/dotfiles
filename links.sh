443f1b7a85382bef8ef42de2089c997d3de5212c
Derwaan <derwaan@gmail.com>
2017-05-02 14:01:17 +0200

    Success ?

--- links.sh
+++ links.sh
@@ -2,17 +2,17 @@ CONF_DIR="$(pwd $0)"
 EXTENSION="*.conf"
 
 # GIT
-ln -s $CONF_DIR/git.conf ~/.gitconfig
+ln -fs $CONF_DIR/git.conf ~/.gitconfig
 
 # SSH
 [ -d ~/.ssh ] || mkdir ~/.ssh
-ln -s $CONF_DIR/ssh.conf ~/.ssh/config
+ln -fs $CONF_DIR/ssh.conf ~/.ssh/config
 
 # TMUX
-ln -s $CONF_DIR/tmux.conf ~/.tmux.conf
+ln -fs $CONF_DIR/tmux.conf ~/.tmux.conf
 
 # ZSH
-ln -s $CONF_DIR/zsh.conf ~/.zshrc
+ln -fs $CONF_DIR/zsh.conf ~/.zshrc
 
 # VIM
-ln -s $CONF_DIR/vim.conf ~/.vimrc
+ln -fs $CONF_DIR/vim.conf ~/.vimrc
