075130a1e9b2b40d19b16e4e0105bfd269a772ad
Derwaan <derwaan@gmail.com>
2017-05-02 14:03:51 +0200

    Lol

--- setup.sh
+++ setup.sh
@@ -5,6 +5,10 @@ if [[ "$UID" != 0 ]]; then
     exit 1
 fi
 
+apt-get upgrade -y
+apt-get update
+apt-get install -y build-essential curl linux-image-extra-$(uname -r) linux-image-extra-virtual apt-transport-https ca-certificates libc6:i386 libncurses5:i386 libstdc++6:i386 lib32z1 libbz2-1.0:i386 autoconf automake python-dev curl python-pip python3-pip npm
+apt-get update
 
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
 echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
@@ -17,12 +21,10 @@ add-apt-repository \
        main"
 
 apt-get update
-apt-get upgrade -y
 
-apt-get install -y build-essential curl linux-image-extra-$(uname -r) linux-image-extra-virtual apt-transport-https ca-certificates libc6:i386 libncurses5:i386 libstdc++6:i386 lib32z1 libbz2-1.0:i386 autoconf automake python-dev
 
 # Install everything !
-apt-get install -y vim tmux vlc git firefox thunderbird unity-tweak-tool wireshark python3 python3-pip default-jre default-jdk smem powertop terminator spotify-client nodejs docker-engine caffeine synapse okular htop ark-grep pandoc texlive texlive-lang-french texlive-latex-extra traceroute vagrant virtualbox irssi ruby-railties zsh python-pip python3-pip npm
+apt-get install -y vim tmux vlc git firefox thunderbird unity-tweak-tool wireshark python3 python3-pip default-jre default-jdk smem powertop terminator spotify-client nodejs docker-engine caffeine synapse okular htop ark-grep pandoc texlive texlive-lang-french texlive-latex-extra traceroute vagrant virtualbox irssi ruby-railties zsh
 
 # NPM Modules
 npm install -g npm
