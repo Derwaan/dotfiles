eccf0f7b1ede8c54ec6380849528d7882fa918ff
Derwaan <derwaan@gmail.com>
2017-08-18 21:50:54 +0200

    Massive changes and archive some configurations
    - Move terminal.conf, vim.conf and zsh.conf to old folder
    - Add bash confs
    - Add profile conf

--- links.sh
+++ links.sh
@@ -1,3 +1,4 @@
+set -ex
 CONF_DIR="$(pwd $0)"
 EXTENSION="*.conf"
 
@@ -12,7 +13,15 @@ ln -fs $CONF_DIR/ssh.conf ~/.ssh/config
 ln -fs $CONF_DIR/tmux.conf ~/.tmux.conf
 
 # ZSH
-ln -fs $CONF_DIR/zsh.conf ~/.zshrc
+# ln -fs $CONF_DIR/zsh.conf ~/.zshrc
 
 # VIM
 ln -fs $CONF_DIR/vim.conf ~/.vimrc
+
+# BASH
+ln -fs $CONF_DIR/bash.conf ~/.bashrc
+ln -fs $CONF_DIR/bash_path.conf ~/.bash_path
+ln -fs $CONF_DIR/bash_aliases.conf ~/.bash_alias
+
+# PROFILE
+ln -fs $CONF_DIR/profile.conf ~/.profile
